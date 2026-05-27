aed5f5dfd10755a57e4b8d846f9560e7c3474e04
damian <damian>
2026-05-27 20:31:18 +0200

    aws ci/cd

--- frontend/app_modules/views/common.py
+++ frontend/app_modules/views/common.py
@@ -3,7 +3,7 @@ import streamlit as st
 def render_header():
     st.markdown("""
         <div style='text-align: center; padding: 2rem 0;'>
-            <h1>🌍 Intelligent Trip Planner test</h1>
+            <h1>🌍 Intelligent Trip Planner tes</h1>
             <p style='color: #6b7280; font-size: 1.1rem;'>Your personal AI-created journey</p>
         </div>
     """, unsafe_allow_html=True)
